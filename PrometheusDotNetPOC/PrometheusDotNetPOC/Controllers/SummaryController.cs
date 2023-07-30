d22b76ead10a8a626e98d157efd31ce812ae9223
IgorCavalcantiCianniPrado <igorcianni@yahoo.com.br>
2023-07-30 00:34:16 -0300

    Added Summary metric

---
+++ PrometheusDotNetPOC/PrometheusDotNetPOC/Controllers/SummaryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Prometheus;
+
+namespace PrometheusDotNetPOC.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SummaryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            //Unsderstand better this metric and how it works in the prometheus-net lib
+            var RequestSizeSummary = Metrics.CreateSummary("api_summary", "Summary in the last 10 minutes.", new SummaryConfiguration
+            {
+                MaxAge = TimeSpan.FromSeconds(10)
+            });
+
+            RequestSizeSummary.Observe(200);
+
+            return Ok("The Prometheus Summary Metric was generated");
+        }
+    }
+}
